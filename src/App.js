7bd90c436f3545df93de5fcba5a08e65b7a6fc11
hyeonseo <gustj7038@gmail.com>
2025-02-03 13:41:43 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,30 +6,30 @@ import './styles.css';
 
 const App = () => {
     return (
-        <Router basename={`${process.env.PUBLIC_URL}`}>
+        <Router basename={process.env.PUBLIC_URL}>
             <nav>
                 <ul className="nav-list">
                     <li>
-                        <Link to={`/`} className="nav-link">
+                        <Link to="/" className="nav-link">
                             홈
                         </Link>
                     </li>
                     <li>
-                        <Link to={`/add`} className="nav-link">
+                        <Link to="/add" className="nav-link">
                             새 회차 추가
                         </Link>
                     </li>
                     <li>
-                        <Link to={`/stats`} className="nav-link">
+                        <Link to="/stats" className="nav-link">
                             재방문 통계
                         </Link>
                     </li>
                 </ul>
             </nav>
             <Routes>
-                <Route path={`/`} element={<MainPage />} />
-                <Route path={`/add`} element={<AddSessionPage />} />
-                <Route path={`/stats`} element={<RevisitStatsPage />} />
+                <Route path="/" element={<MainPage />} />
+                <Route path="/add" element={<AddSessionPage />} />
+                <Route path="/stats" element={<RevisitStatsPage />} />
             </Routes>
         </Router>
     );
