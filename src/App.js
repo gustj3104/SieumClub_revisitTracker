ac2c5c8e6baba69319262bec9e5bb2e438fc2825
hyeonseo <gustj7038@gmail.com>
2025-02-03 10:16:11 +0900

    Fix: Link, Path Update

--- src/App.js
+++ src/App.js
@@ -10,26 +10,26 @@ const App = () => {
             <nav>
                 <ul className="nav-list">
                     <li>
-                        <Link to="/" className="nav-link">
+                        <Link to={process.env.PUBLIC_URL + '/'} className="nav-link">
                             홈
                         </Link>
                     </li>
                     <li>
-                        <Link to="/add" className="nav-link">
+                        <Link to={process.env.PUBLIC_URL + '/add'} className="nav-link">
                             새 회차 추가
                         </Link>
                     </li>
                     <li>
-                        <Link to="/stats" className="nav-link">
+                        <Link to={process.env.PUBLIC_URL + '/stats'} className="nav-link">
                             재방문 통계
                         </Link>
                     </li>
                 </ul>
             </nav>
             <Routes>
-                <Route path="/" element={<MainPage />} />
-                <Route path="/add" element={<AddSessionPage />} />
-                <Route path="/stats" element={<RevisitStatsPage />} />
+                <Route path={process.env.PUBLIC_URL + '/'} element={<MainPage />} />
+                <Route path={process.env.PUBLIC_URL + '/add'} element={<AddSessionPage />} />
+                <Route path={process.env.PUBLIC_URL + '/stats'} element={<RevisitStatsPage />} />
             </Routes>
         </Router>
     );
